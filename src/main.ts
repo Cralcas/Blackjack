312647b55a147d10b493dd46c99338faba742007
Cralcas <carlosbergq@gmail.com>
2024-03-09 17:33:02 +0100

    Created object for player name and chips

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import "./style.css";
+
+let player: { name: string; chips: number } = {
+  name: "Player",
+  chips: 200,
+};
+
+// let cards: [] = [];
+// console.log(player);
+// let messageEl = document.getElementById("message-el");
+// let sumEl = document.getElementById("sum-el");
+// let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el") as HTMLElement;
+
+playerEl.innerText = player.name + ": $" + player.chips;
